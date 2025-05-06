e9646a0ed82962c6142853da6e2355e817f9923a
Aleksei Drobkov <abkommem@gmail.com>
2025-05-06 16:28:14 +0700

    backend

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const serverCode = require('../server.js');
+
+module.exports = serverCode; 
